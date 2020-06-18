b25050c167694acdf23792130f7c941c16e99141
MateusVS <mateusviniciussilva07@gmail.com>
2020-06-18 13:21:11 -0300

    Criação grid do layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import Layout from './Components/Layout';
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
+      <Layout />
       <GlobalStyles />
-      <h1>Rocketseat</h1>
     </>
   );
 }
