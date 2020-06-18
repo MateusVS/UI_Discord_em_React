61c36a1db82f4d91e1c3c9a6e5edcc607d92019c
MateusVS <mateusviniciussilva07@gmail.com>
2020-06-18 18:06:22 -0300

    Criação Lista de canais

---
+++ src/Components/ChannelList/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import ChannelButton from '../ChannelButton';
+
+import { Container, Category, AddCategoryIcon } from './styles';
+
+const ChannelList: React.FC = () => {
+    return (
+        <Container>
+            <Category>
+                <span>Canais de texto</span>
+                <AddCategoryIcon />
+            </Category>
+
+            <ChannelButton channelName="chat-livre" />
+            <ChannelButton channelName="Trabalho" />
+            <ChannelButton channelName="Dota 2" />
+            <ChannelButton channelName="CS GO" />
+            <ChannelButton channelName="Valorant" />
+        </Container>
+    )
+};
+
+export default ChannelList;
